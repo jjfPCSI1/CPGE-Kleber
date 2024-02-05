9f354920428b980a4755ca844e501b516cf085b1
JJ Fleck <jeanjulien.fleck@gmail.com>
2024-02-05 15:34:13 +0100

    :tada: Arrivée de la série des TP-express

--- fabrique_sous_pages.py
+++ fabrique_sous_pages.py
@@ -38,19 +38,7 @@ def lecture_donnees(fichier):
             videos.append(row)
         return videos, matieres, types
 
-CHAP_PHYS = lecture_chap('chapitres_physique.csv')
 
-CHAP = {'Physique': CHAP_PHYS}
-
-DONNEES_VIDEOS, MATIERES, TYPES = lecture_donnees('donnees_videos.csv')
-
-# Création des différents répertoires
-import os
-for matiere in MATIERES:
-    if not os.path.exists(matiere):
-         os.mkdir(matiere)
-         print('Création du répertoire {}/'.format(matiere))
-    
 
 def embedded_video(lien):
     video_inline = lien.replace('https://youtu.be/', 'https://www.youtube.com/embed/')
@@ -99,62 +87,78 @@ def fabrique_page_chapitre(chapitre, matiere='Physique'):
             if len(vids) > 0:
                  f.write(partie_page(ASSOC_TYPES[type], vids))
 
-# Exécution proprement dite
-for matiere in CHAP:
-    for chap in CHAP[matiere]:
-        fabrique_page_chapitre(chap, matiere)
+if __name__ == '__main__':
+
+    CHAP_PHYS = lecture_chap('chapitres_physique.csv')
+
+    CHAP = {'Physique': CHAP_PHYS}
+
+    DONNEES_VIDEOS, MATIERES, TYPES = lecture_donnees('donnees_videos.csv')
+
+    # Création des différents répertoires
+    import os
+    for matiere in MATIERES:
+        if not os.path.exists(matiere):
+            os.mkdir(matiere)
+            print('Création du répertoire {}/'.format(matiere))
 
 
-# On fabrique aussi les pages principales de chaque matière en complétant le 
-# fichier .txt déjà présent avec la liste des chapitres disponibles
+    # Exécution proprement dite
+    for matiere in CHAP:
+        for chap in CHAP[matiere]:
+            fabrique_page_chapitre(chap, matiere)
 
-for matiere in CHAP:
-    mat = matiere.lower()
-    s = ''
-    with open('{}.txt'.format(mat)) as f:
-         for r in f:
-             s += r
-    generic = '* [{}]({}/{}.html) \n'
-    with open('{}.md'.format(mat), 'w') as f:
-        try:
-             chapitres = CHAP[matiere]
-             chaps = list(chapitres.keys())
-             print(chaps)
-             chaps.sort(key=lambda c: chapitres[c]['Ordre'])
-             bloc = None
-             for c in chaps:
-                 if not(bloc) or bloc != chapitres[c]['Bloc']: 
-                    bloc = chapitres[c]['Bloc']
-                    s += '\n\n### {}\n\n'.format(bloc)
-                 print(c)
-                 s+= generic.format(chapitres[c]['Titre'], matiere, c)
-             s += '\n\n'
-        except : 
-            raise 
-        f.write(s)
-    # Pour chaque matière, on regarde aussi les pages spécifiques pour chaque type de vidéo
-    for type in ASSOC_TYPES.keys():
-        videos = [v for v in DONNEES_VIDEOS if v['Type'] == type and v['Matière'] == matiere]
-        if len(videos) > 0:
-            s = '# {} en {}\n\n'.format(ASSOC_TYPES[type], matiere)
+
+    # On fabrique aussi les pages principales de chaque matière en complétant le
+    # fichier .txt déjà présent avec la liste des chapitres disponibles
+
+    for matiere in CHAP:
+        mat = matiere.lower()
+        s = ''
+        with open('{}.txt'.format(mat)) as f:
+            for r in f:
+                s += r
+        generic = '* [{}]({}/{}.html) \n'
+        with open('{}.md'.format(mat), 'w') as f:
             try:
-                 chapitres = CHAP[matiere]
-                 chaps = list(chapitres.keys())
-#                 print(chaps, type)
-                 chaps.sort(key=lambda c: chapitres[c]['Ordre'])
-                 bloc = None
-                 for c in chaps:
-                     v_chaps = [v for v in videos if v['Chapitre'] == c]
-                     if len(v_chaps) == 0: continue
-                     if not(bloc) or bloc != chapitres[c]['Bloc']: 
+                chapitres = CHAP[matiere]
+                chaps = list(chapitres.keys())
+                print(chaps)
+                chaps.sort(key=lambda c: chapitres[c]['Ordre'])
+                bloc = None
+                for c in chaps:
+                    if not(bloc) or bloc != chapitres[c]['Bloc']:
                         bloc = chapitres[c]['Bloc']
                         s += '\n\n### {}\n\n'.format(bloc)
-                     print(c, type)
-                     for v in v_chaps:
-                         s+= '* [{}]({})\n'.format(v['Titre'], v['Lien'])
-                         s+= embedded_video(v['Lien'])
-                 s += '\n\n'
-            except : 
-                raise 
-            with open('{}/{}.md'.format(matiere, type), 'w') as f:
-                 f.write(s)
+                    print(c)
+                    s+= generic.format(chapitres[c]['Titre'], matiere, c)
+                s += '\n\n'
+            except :
+                raise
+            f.write(s)
+        # Pour chaque matière, on regarde aussi les pages spécifiques pour chaque type de vidéo
+        for type in ASSOC_TYPES.keys():
+            videos = [v for v in DONNEES_VIDEOS if v['Type'] == type and v['Matière'] == matiere]
+            if len(videos) > 0:
+                s = '# {} en {}\n\n'.format(ASSOC_TYPES[type], matiere)
+                try:
+                    chapitres = CHAP[matiere]
+                    chaps = list(chapitres.keys())
+    #                 print(chaps, type)
+                    chaps.sort(key=lambda c: chapitres[c]['Ordre'])
+                    bloc = None
+                    for c in chaps:
+                        v_chaps = [v for v in videos if v['Chapitre'] == c]
+                        if len(v_chaps) == 0: continue
+                        if not(bloc) or bloc != chapitres[c]['Bloc']:
+                            bloc = chapitres[c]['Bloc']
+                            s += '\n\n### {}\n\n'.format(bloc)
+                        print(c, type)
+                        for v in v_chaps:
+                            s+= '* [{}]({})\n'.format(v['Titre'], v['Lien'])
+                            s+= embedded_video(v['Lien'])
+                    s += '\n\n'
+                except :
+                    raise
+                with open('{}/{}.md'.format(matiere, type), 'w') as f:
+                    f.write(s)
